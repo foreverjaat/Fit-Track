b11ad0a4c2cf12dec69518ba52c73e874d7cc1d5
Anuj Chaudhary <chaudharyanuj0123@gmail.com>
2026-03-21 20:02:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || `https://fit-track-frontend-jit0.onrender.com`, credentials: true }));
+app.use(cors({ origin:  `https://fit-track-frontend-jit0.onrender.com`, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
